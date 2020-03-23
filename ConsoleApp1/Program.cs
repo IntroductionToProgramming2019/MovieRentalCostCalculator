cfb55a07d3f189feabe42e107e59de8815533be4
Johnathon Crawford <47326832+Kodi125@users.noreply.github.com>
2020-03-23 17:35:10 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿/* #############################
+ * 
+ * Author: Johnathon Mc Grory
+ * Date :
+ * Description : complete streamreading and writing guide
+ * 
+ * ############################# */
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cost;
+            const double PRICE_PER_3_DAYS = 2;
+            const double PRICE_MORE_THAN_THREE_DAYS = 1.5;
+            const double MAX_COST = 50;
+            Console.WriteLine("Please enter the number of days you have the dvd for >>");
+            int days = int.Parse(Console.ReadLine());
+
+
+            
+                if (days > 3)
+            { 
+                    cost = (3 * PRICE_MORE_THAN_THREE_DAYS) + (days - 3) * PRICE_MORE_THAN_THREE_DAYS;
+            } 
+
+            else 
+            {
+                cost = (days * PRICE_PER_3_DAYS);
+            }
+            if (cost > MAX_COST)
+                {
+                cost = MAX_COST;
+            }
+            Console.WriteLine("Cost = {0:c}", cost);
+        }
+    }
+}
